30a7c5119da157009348252cc6204e06c387731d
Parth Bhakta <bhaktaparthh@gmail.com>
2022-03-03 16:38:39 -0700

    backend-server-established

--- backend/app.js
+++ backend/app.js
@@ -40,4 +40,23 @@ if (!isProduction) {
 
   app.use(routes);
 
+  app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+  });
+
+  app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+      stack: isProduction ? null : err.stack
+    });
+  });
+
   module.exports = app;
